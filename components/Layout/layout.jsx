b991558204745c57a172eb846bc068253a084d3c
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-10-21 20:28:32 -0300

    ts files deleted

--- components/Layout/layout.jsx
+++ components/Layout/layout.jsx
@@ -1,12 +1,7 @@
 import Header from "../Header/header";
 import Footer from "../Footer/footer";
-import { MunicipioContext } from "../../src/hooks/useMunicipio";
-import { useContext } from "react";
 
 export default function Layout({ children }) {
-  // const data = useContext(MunicipioContext);
-  // console.log(data);
-
   return (
     <>
       <Header />
