77e9307b817c879c5b8ba5fafea16e6529910ac2
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-10-23 21:37:33 -0300

    Fixing policy http

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <meta
-            http-equiv="Content-Security-Policy"
+            httpEquiv="Content-Security-Policy"
             content="upgrade-insecure-requests"
           />
         </Head>
