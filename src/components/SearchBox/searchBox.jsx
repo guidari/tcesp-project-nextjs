dc73e2f601294ab4f015e5af9f5930764bc81e4d
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-10-25 19:01:51 -0300

    Arrumando datas

--- src/components/SearchBox/searchBox.jsx
+++ src/components/SearchBox/searchBox.jsx
@@ -48,6 +48,16 @@ export default function SearchBox() {
           <option value="" disabled defaultValue>
             Selecione o ano
           </option>
+          <option value="2010">2010</option>
+          <option value="2011">2011</option>
+          <option value="2012">2012</option>
+          <option value="2013">2013</option>
+          <option value="2014">2014</option>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
           <option value="2020">2020</option>
           <option value="2021">2021</option>
         </select>
