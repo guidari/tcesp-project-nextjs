b73cdcaa2afe049d9f0e4e88450f86e3467e95ba
BielChiode <gabrielchiode@hotmail.com>
2021-10-25 17:27:30 -0300

    tabelas de despesas e receitas

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,15 @@
 import { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
-
+import Head from "next/head";
 import "../styles/global.scss";
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
